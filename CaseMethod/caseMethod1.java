933596551e369df8e6070d7b8d84ff23fd18833a
mov1cc <imovic004@gmail.com>
2025-10-18 23:31:03 +0700

    Case Method 1

---
+++ CaseMethod/caseMethod1.java
@@ -0,0 +1,22 @@
+package CaseMethod;
+public class caseMethod1 {
+    public static void main(String[]args){
+        
+        String nama="Ibrahim Movic";
+        long nim=254107020158L;
+        String jurusan="Teknik Informatika";
+        int semester=3;
+        double ipk=3.85;
+        String status="Cumlaude";
+        
+        System.out.println("======DATA MAHASISWA======");
+        System.out.println("NAMA: "+nama);
+        System.out.println("NIM: "+nim);
+        System.out.println("JURUSAN: "+jurusan);
+        System.out.println("SEMESTER: "+semester);
+        System.out.println("IPK: "+ipk);
+        System.out.println("STATUS: "+status);
+        System.out.println("==========================");
+        
+    }
+}
