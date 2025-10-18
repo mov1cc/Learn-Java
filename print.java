e83a19502a83614ab33f702e5eab74237d211c3b
mov1cc <imovic004@gmail.com>
2025-10-18 22:49:04 +0700

    Print, Println, and \n

---
+++ print.java
@@ -0,0 +1,16 @@
+public class print {
+    public static void main(String[]args){
+        
+        System.out.print("Halo everyone this is my first code in java");
+        System.out.println("Hai");
+        System.out.print("My name is Movic\n");
+        System.out.print("Good Evening");
+        
+    }
+}
+/*
+System.out.print adalah
+sytem.out.println adalah
+\n adalah 
+Menambahkan comment di Java menghunakan //
+Menambahkan comment di Java menggunakan /* */
