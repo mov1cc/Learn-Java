8b38e515fbd713111bdc7bae085c2c5b932a9ed9
mov1cc <imovic004@gmail.com>
2025-10-19 14:43:17 +0700

    User Input 2

---
+++ userInput2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class userInput2 {
+    public static void main(String[]args){
+        
+        double width=0;
+        double height=0;
+        double area=0;
+        
+        Scanner scanner=new Scanner(System.in);
+        
+        System.out.print("Enter Width: ");
+        
+        width=scanner.nextDouble();
+        
+        System.out.print("Enter Height: ");
+        
+        height=scanner.nextDouble();
+        
+        area=width*height;
+        
+        System.out.println("The area is: "+area+"cm²");
+        
+        
+        scanner.close();
+    }
+}
