e2fb2b5ddae3e5ac9fd9a6fb8e6c9641229ed792
movic <136174574+mov1cc@users.noreply.github.com>
2025-10-22 11:07:18 +0700

    Create ternaryOperator.java

---
+++ ternaryOperator.java
@@ -0,0 +1,8 @@
+public class ternaryOperator {
+public static void main(String[]args){ 
+int nilai = 75;
+String status = (nilai >= 60) ? "Lulus" : "Tidak Lulus";
+
+System.out.println("Status: " + status);
+}
+}
