daac184994eec744a7998ecd0c58ba0622585f62
mov1cc <imovic004@gmail.com>
2025-11-18 15:53:32 +0700

    lupa format biar rapi hihi

--- BasicJava/Array.java
+++ BasicJava/Array.java
@@ -1,28 +1,33 @@
 package BasicJava;
+
 import java.util.Arrays;
 
 public class Array {
-    public static void main(String[]args){
-        // A COLLECTION VALUES OF THE SAME DATA TYPE (KUMPULAN NILAI DENGAN TIPE DATA YANG SAMA)
-        // *THINK OF IT AS A VARIABLE THAT CAN STORE MORE THAN 1 VALUE (BAYANGKAN SATU VARIABEL DAPAT MENYIMPAN LEBIH DARI SATU NILAI DENGAN TIPE DATA YANG SAMA)
+    public static void main(String[] args) {
+        // A COLLECTION VALUES OF THE SAME DATA TYPE (KUMPULAN NILAI DENGAN TIPE DATA
+        // YANG SAMA)
+        // *THINK OF IT AS A VARIABLE THAT CAN STORE MORE THAN 1 VALUE (BAYANGKAN SATU
+        // VARIABEL DAPAT MENYIMPAN LEBIH DARI SATU NILAI DENGAN TIPE DATA YANG SAMA)
 
-        String fruits[] = {"Banana", "Apple", "Strawberry"};
-        //fruits[0]="Pinnaple";
-        //int num = fruits.length;
-        //Arrays.sort(fruits);
+        String fruits[] = { "Banana", "Apple", "Strawberry" };
+        // fruits[0]="Pinnaple";
+        // int num = fruits.length;
+        // Arrays.sort(fruits);
         Arrays.fill(fruits, "Pineapple");
 
         // System.out.println(fruits[0]);
         // System.out.println(num);
 
-        /*for(int i=0;i<fruits.length;i++){
-            System.out.print(fruits[i]+" ");
-        }*/
+        /*
+         * for(int i=0;i<fruits.length;i++){
+         * System.out.print(fruits[i]+" ");
+         * }
+         */
 
         // ENHANCE FOR LOOP
-        for(String fruit : fruits){
+        for (String fruit : fruits) {
             System.out.println(fruit);
-        } 
-            
+        }
+
     }
 }
